c69d665afb4cd97c060e74068e56bf6d7525fbf7
Mustafa Aba <mustafa_aba@outlook.com>
2023-06-22 15:19:28 +0300

    .

--- src/j100_CodeChallange/Challange08/Task01.java
+++ src/j100_CodeChallange/Challange08/Task01.java
@@ -1,5 +1,7 @@
 package j100_CodeChallange.Challange08;
 
+import java.util.Arrays;
+
 public class Task01 {
     public static void main(String[] args) {
 		/* task->
@@ -9,7 +11,10 @@ public class Task01 {
         */
 
         int arr[] = {2,6,4,5,8,9};
-
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=(int)Math.pow(arr[i],2);
+        }
+        System.out.println("arr = " + Arrays.toString(arr));
 
     }
 }
