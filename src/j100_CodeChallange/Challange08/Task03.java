f8c6ab46b2d12a1e479111b1435dc06b8b8dafbd
Mustafa Aba <mustafa_aba@outlook.com>
2023-06-22 15:12:54 +0300

    .

---
+++ src/j100_CodeChallange/Challange08/Task03.java
@@ -0,0 +1,14 @@
+package j100_CodeChallange.Challange08;
+
+
+public class Task03 {
+
+    public static void main(String[] args) {
+        /* task->
+         *(String iniz: "HeySiri"  Diziyi "ByeSiri" ye cevirin )
+         *
+         */
+
+
+    }
+}
---
+++ src/j100_CodeChallange/Challange08/Task05.java
@@ -0,0 +1,12 @@
+package j100_CodeChallange.Challange08;
+
+public class Task05 {
+    public static void main(String[] args) {
+       /* task->
+         Kullanicidan aldigimiz 8 elemanli arrayin icinde
+         kac tane 3 e bolunebilen sayi vardir ?(negatif sayilar da dahil olsun)
+        */
+
+
+    }
+}
